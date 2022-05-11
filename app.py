8bd48722e1f57eddcc8808a78f5f008f5e8c643b
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-05-11 21:31:38 +0900

    로그인 회원가입 페이지 / app.py 코드 추가

--- app.py
+++ app.py
@@ -1,24 +1,93 @@
-from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
-from pymongo import MongoClient
-import certifi
+SECRET_KEY = 'SPARTA'
 
-ca = certifi.where()
-client = MongoClient('mongodb+srv://test:sparta@cluster0.zuy17.mongodb.net/Cluster0?retryWrites=true&w=majority',
-                     tlsCAFile=ca)
-db = client.dbsparta
+client = MongoClient('13.125.6.243', 27017, username="test", password="test")
+db = client.dbsparta_Todays_Vibe
 
 
-# 여기부터 코드 작성
+# 여기부터 코드 작성 #
 
 @app.route('/')
 def home ():
-    return render_template('index.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-# 여기까지 코드 작성
+@app.route('/login')
+def login ():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+# 로그인
+@app.route('/sign_in', methods=['POST'])
+def sign_in ():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+# 회원가입
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up ():
+    # 입력받는 데이터
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    # DB에 저장
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": ""  # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup ():
+    # ID 중복확인
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+# 여기까지 코드 작성 #
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
