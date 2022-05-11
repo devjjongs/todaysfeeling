780d5d4701f8f8a4c903a43124223e0d5f6fc9a3
treblabin <treblabin@gmail.com>
2022-05-11 18:39:32 +0900

    이미지 static/image에 저장하는 방식으로 바꿈. 정상작동.

--- app1.py
+++ app1.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
+from datetime import datetime
 
 client = MongoClient('13.125.6.243', 27017, username="test", password="test")
 db = client.dbsparta_Todays_Vibe
@@ -12,35 +13,46 @@ db = client.dbsparta_Todays_Vibe
 
 @app.route('/')
 def home ():
-    return render_template('index.html')
+    return render_template('/index.html')
 
 @app.route('/post')
 def post ():
-    return render_template('post.html')
+    return render_template('/post.html')
 
 @app.route("/writing", methods=["POST"])
 def post_upload():
     # token_receive = requestuest.cookies.get('mytoken')
-    try:
+    # try:
         # payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         # user_info = db.users.find_one({"username": payload["id"]})
-        date_receive = request.form['date_give']
+        today = datetime.now()
+        mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
         weather_receive = request.form['weather_give']
         word_receive = request.form['word_give']
         tag_receive = request.form['tag_give']
 
+        file = request.files['file_give']
+        extension = file.filename.split('.')[-1]
+        filename = f'file-{mytime}'
+        save_to = f'static/images/{filename}.{extension}'
+
+        file.save(save_to)
+
+        # 사용자 ID 추가하는 기능 필요
+
         doc = {
             # 'username': user_info["username"],
-            'date': date_receive,
+            'today': mytime,
             'weather': weather_receive,
             'word': word_receive,
-            'tag': tag_receive
+            'tag': tag_receive,
+            'file': f'{filename}.{extension}'
         }
 
         db.writes.insert_one(doc)
         return jsonify({"result": "success", 'msg': '포스팅이 완료되었습니다.'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+    # except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+    #     return redirect(url_for("home"))
 
 # 여기까지 코드 작성
 
